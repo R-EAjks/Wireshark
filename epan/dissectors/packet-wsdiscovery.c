c7084b550046e5dd5ee7639242c6aa1fd9ee479f
Masaru Tsuchiyama <m.tmatma@gmail.com>
2021-02-12 10:16:57 +0900

    add dissectors for WS-discovery.

---
+++ epan/dissectors/packet-wsdiscovery.c
@@ -0,0 +1,61 @@
+/* packet-wsdiscovery.c
+ *
+ * Wireshark - Network traffic analyzer
+ * By Gerald Combs <gerald@wireshark.org>
+ * Copyright 1998 Gerald Combs
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ *
+ */
+
+#include "config.h"
+#include <epan/packet.h>
+
+#define WS_DISCOVERY_PORT 3702
+
+static int proto_wsdiscovery = -1;
+static dissector_handle_t xml_dissector_handle;
+
+static int
+dissect_wsdiscovery(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree _U_, void *data _U_)
+{
+    col_set_str(pinfo->cinfo, COL_PROTOCOL, "WS-Discovery");
+    /* Clear the info column */
+    col_clear(pinfo->cinfo,COL_INFO);
+
+    return call_dissector(xml_dissector_handle, tvb, pinfo, tree);
+}
+
+void
+proto_register_wsdiscovery(void)
+{
+    proto_wsdiscovery = proto_register_protocol (
+        "WS-Discovery",  /* name        */
+        "WSDISCOVERY",  /* short_name  */
+        "wsdiscovery"   /* filter_name */
+        );
+}
+
+void
+proto_reg_handoff_wsdiscovery(void)
+{
+    static dissector_handle_t wsdiscovery_handle;
+
+    wsdiscovery_handle = create_dissector_handle(dissect_wsdiscovery, proto_wsdiscovery);
+    dissector_add_uint("udp.port", WS_DISCOVERY_PORT, wsdiscovery_handle);
+
+    xml_dissector_handle = find_dissector("xml");
+}
+
+/*
+ * Editor modelines  -  https://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 4
+ * tab-width: 8
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * vi: set shiftwidth=4 tabstop=8 expandtab:
+ * :indentSize=4:tabSize=8:noTabs=true:
+ */
