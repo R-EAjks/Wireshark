3e51a034a411e2b8083f44f3903d6094663a55dd
Masaru Tsuchiyama <m.tmatma@gmail.com>
2021-03-07 08:41:39 +0900

    Revert "add `WS-Discovery` Tree Node"

--- epan/dissectors/packet-wsdiscovery.c
+++ epan/dissectors/packet-wsdiscovery.c
@@ -15,37 +15,16 @@
 
 static int proto_wsdiscovery = -1;
 static dissector_handle_t xml_dissector_handle;
-static gint ett_wsdiscovery = -1;
-
-
-static gint* ett[] = {
-    &ett_wsdiscovery,
-};
 
 static int
 dissect_wsdiscovery(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree _U_, void *data _U_)
 {
-    proto_item* protoitem = NULL;
-    proto_tree* discovery_tree = NULL;
-
     col_set_str(pinfo->cinfo, COL_PROTOCOL, "WS-Discovery");
-
-    /* Set the info column */
+    /* Clear the info column */
     col_clear(pinfo->cinfo,COL_INFO);
     col_add_fstr(pinfo->cinfo, COL_INFO, "WS-Discovery");
 
-    /* Create Tree Note for WS-Discovery */
-    protoitem = proto_tree_add_protocol_format(tree, proto_wsdiscovery, tvb, 0, -1, "WS-Discovery");
-    discovery_tree = proto_item_add_subtree(protoitem, ett_wsdiscovery);
-
-    /* Call 'xml' dissector */
-    {
-        tvbuff_t* data_tvb;
-        guint datasize = tvb_captured_length(tvb);
-        const guint8* databuf = tvb_get_ptr(tvb, 0, datasize);
-        data_tvb = tvb_new_child_real_data(tvb, databuf, datasize, datasize);
-        return call_dissector(xml_dissector_handle, data_tvb, pinfo, discovery_tree);
-    }
+    return call_dissector(xml_dissector_handle, tvb, pinfo, tree);
 }
 
 void
@@ -56,9 +35,6 @@ proto_register_wsdiscovery(void)
         "WS-Discovery",                    /* short_name  */
         "wsdiscovery"                      /* filter_name */
     );
-
-    /* Add data partition for expand/unexpand 'wsdiscovery' tree */
-    proto_register_subtree_array(ett, array_length(ett));
 }
 
 void
