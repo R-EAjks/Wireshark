60f881a57df6c1ecb27ac07fc22a34e4a95fc4da
Jeroen Sack <jeroen@jeroensack.nl>
2021-08-26 22:29:47 +0000

    Revert "Fix showing if auxiliary enable message was successful."
    This reverts commit ab1f7169d0662fee3f44343bb36bb3da941a3e2d

--- epan/dissectors/packet-isobus-vt.c
+++ epan/dissectors/packet-isobus-vt.c
@@ -1545,16 +1545,8 @@ dissect_vt(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, enum vt_directio
             }
             else
             {
-                if (status == 0)
-                {
-                    col_append_fstr(pinfo->cinfo, COL_INFO, "Status of Auxiliary Input %s was successfully changed to disabled",
-                        get_object_id_string(auxiliary_input_object_id));
-                }
-                else if (status == 1)
-                {
-                    col_append_fstr(pinfo->cinfo, COL_INFO, "Status of Auxiliary Input %s was successfully changed to enabled",
-                        get_object_id_string(auxiliary_input_object_id));
-                }
+                col_append_fstr(pinfo->cinfo, COL_INFO, "Status of Auxiliary Input %s was successfully changed to enabled",
+                    get_object_id_string(auxiliary_input_object_id));
             }
         }
     }
