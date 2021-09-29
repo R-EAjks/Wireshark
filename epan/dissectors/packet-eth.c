33fcf5156f790bdcf62ed866a0a5f930b218ddd9
Jaap Keuter <jaap.keuter@xs4all.nl>
2021-09-29 18:11:05 +0200

    Eth: Segment routing over IPv6
    Register for Ethernet over IP according to RFC 8986

--- epan/dissectors/packet-eth.c
+++ epan/dissectors/packet-eth.c
@@ -1161,6 +1161,7 @@ proto_reg_handoff_eth(void)
   dissector_add_uint("erf.types.type", ERF_TYPE_COLOR_ETH, eth_maybefcs_handle);
   dissector_add_uint("erf.types.type", ERF_TYPE_DSM_COLOR_ETH, eth_maybefcs_handle);
   dissector_add_uint("erf.types.type", ERF_TYPE_COLOR_HASH_ETH, eth_maybefcs_handle);
+  dissector_add_uint("ip.proto", IP_PROTO_ETHERNET, eth_maybefcs_handle);
 
   dissector_add_uint("chdlc.protocol", ETHERTYPE_ETHBRIDGE, eth_withoutfcs_handle);
   dissector_add_uint("gre.proto", ETHERTYPE_ETHBRIDGE, eth_withoutfcs_handle);
