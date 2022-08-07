R-EAjks/Wireshark

commit 8020aea27a9673eaae0d3d2333b19767c0d6112c

epan: Fix our conversation flags
Make sure we pass NO_ADDR2 and NO_PORT2 to conversation_new and
NO_ADDR_B and NO_PORT_B to find_conversation.

epan/dissectors/packet-umts_fp.c
@@ -4058,7 +4058,7 @@ set_both_sides_umts_fp_conv_data(packet_info *pinfo, umts_fp_conversation_info_t
         /* Conversation does not exist yet, creating one now. */
         packet_direction_conv = conversation_new(pinfo->num, &pinfo->net_dst, &pinfo->net_src,
             conversation_pt_to_endpoint_type(pinfo->ptype),
-            pinfo->destport, pinfo->srcport, NO_ADDR_B);
+            pinfo->destport, pinfo->srcport, NO_PORT2);
     }
     conversation_add_proto_data(packet_direction_conv, proto_fp, umts_fp_conversation_info);
 
@@ -4071,7 +4071,7 @@ set_both_sides_umts_fp_conv_data(packet_info *pinfo, umts_fp_conversation_info_t
         /* Conversation does not exist yet, creating one now. */
         other_direction_conv = conversation_new(pinfo->num, &pinfo->net_src, &pinfo->net_dst,
             conversation_pt_to_endpoint_type(pinfo->ptype),
-            pinfo->srcport, pinfo->destport, NO_ADDR_B);
+            pinfo->srcport, pinfo->destport, NO_PORT2);
     }
     conversation_add_proto_data(other_direction_conv, proto_fp, umts_fp_conversation_info);
 

epan/dissectors/packet-dof.c
@@ -5720,7 +5720,7 @@ static int dissect_dof_udp(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, 
         conversation = find_conversation(pinfo->fd->num, &pinfo->src, &pinfo->dst, conversation_pt_to_endpoint_type(pinfo->ptype), pinfo->srcport, pinfo->destport, NO_ADDR_B | NO_PORT_B);
         if (!conversation)
         {
-            conversation = conversation_new(pinfo->fd->num, &pinfo->src, &pinfo->dst, conversation_pt_to_endpoint_type(pinfo->ptype), pinfo->srcport, pinfo->destport, NO_ADDR_B | NO_PORT_B);
+            conversation = conversation_new(pinfo->fd->num, &pinfo->src, &pinfo->dst, conversation_pt_to_endpoint_type(pinfo->ptype), pinfo->srcport, pinfo->destport, NO_ADDR2 | NO_PORT2);
             conversation_set_dissector(conversation, dof_udp_handle);
         }
 

epan/dissectors/packet-fcdns.c
@@ -1473,7 +1473,7 @@ dissect_fcdns (tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void* data)
     if ((opcode != FCCT_MSG_ACC) && (opcode != FCCT_MSG_RJT)) {
         conversation = find_conversation (pinfo->num, &pinfo->src, &pinfo->dst,
                                           conversation_pt_to_endpoint_type(pinfo->ptype), fchdr->oxid,
-                                          fchdr->rxid, NO_PORT2);
+                                          fchdr->rxid, NO_PORT_B);
         if (!conversation) {
             conversation = conversation_new (pinfo->num, &pinfo->src, &pinfo->dst,
                                              conversation_pt_to_endpoint_type(pinfo->ptype), fchdr->oxid,
@@ -1507,7 +1507,7 @@ dissect_fcdns (tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void* data)
         /* Opcode is ACC or RJT */
         conversation = find_conversation (pinfo->num, &pinfo->src, &pinfo->dst,
                                           conversation_pt_to_endpoint_type(pinfo->ptype), fchdr->oxid,
-                                          fchdr->rxid, NO_PORT2);
+                                          fchdr->rxid, NO_PORT_B);
         isreq = 0;
         if (!conversation) {
             if (opcode == FCCT_MSG_ACC) {

epan/dissectors/packet-fcfcs.c
@@ -702,7 +702,7 @@ dissect_fcfcs (tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void* data)
     if ((opcode != FCCT_MSG_ACC) && (opcode != FCCT_MSG_RJT)) {
         conversation = find_conversation (pinfo->num, &pinfo->src, &pinfo->dst,
                                           conversation_pt_to_endpoint_type(pinfo->ptype), fchdr->oxid,
-                                          fchdr->rxid, NO_PORT2);
+                                          fchdr->rxid, NO_PORT_B);
         if (!conversation) {
             conversation = conversation_new (pinfo->num, &pinfo->src, &pinfo->dst,
                                              conversation_pt_to_endpoint_type(pinfo->ptype), fchdr->oxid,
@@ -736,7 +736,7 @@ dissect_fcfcs (tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void* data)
         /* Opcode is ACC or RJT */
         conversation = find_conversation (pinfo->num, &pinfo->src, &pinfo->dst,
                                           conversation_pt_to_endpoint_type(pinfo->ptype), fchdr->oxid,
-                                          fchdr->rxid, NO_PORT2);
+                                          fchdr->rxid, NO_PORT_B);
         isreq = 0;
         if (!conversation) {
             if (opcode == FCCT_MSG_ACC) {

epan/dissectors/packet-fcfzs.c
@@ -525,7 +525,7 @@ dissect_fcfzs(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void* data)
     if ((opcode != FCCT_MSG_ACC) && (opcode != FCCT_MSG_RJT)) {
         conversation = find_conversation(pinfo->num, &pinfo->src, &pinfo->dst,
                                          conversation_pt_to_endpoint_type(pinfo->ptype), fchdr->oxid,
-                                         fchdr->rxid, NO_PORT2);
+                                         fchdr->rxid, NO_PORT_B);
         if (!conversation) {
             conversation = conversation_new(pinfo->num, &pinfo->src, &pinfo->dst,
                                             conversation_pt_to_endpoint_type(pinfo->ptype), fchdr->oxid,
@@ -560,7 +560,7 @@ dissect_fcfzs(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void* data)
         /* Opcode is ACC or RJT */
         conversation = find_conversation(pinfo->num, &pinfo->src, &pinfo->dst,
                                          conversation_pt_to_endpoint_type(pinfo->ptype), fchdr->oxid,
-                                         fchdr->rxid, NO_PORT2);
+                                         fchdr->rxid, NO_PORT_B);
         isreq = FALSE;
         if (!conversation) {
             if (opcode == FCCT_MSG_ACC) {

epan/dissectors/packet-fcswils.c
@@ -1681,7 +1681,7 @@ dissect_fcswils(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void* data)
     if ((opcode != FC_SWILS_SWACC) && (opcode != FC_SWILS_SWRJT)) {
         conversation = find_conversation(pinfo->num, &pinfo->src, &pinfo->dst,
                                          conversation_pt_to_endpoint_type(pinfo->ptype), fchdr->oxid,
-                                         fchdr->rxid, NO_PORT2);
+                                         fchdr->rxid, NO_PORT_B);
         if (!conversation) {
             conversation = conversation_new(pinfo->num, &pinfo->src, &pinfo->dst,
                                             conversation_pt_to_endpoint_type(pinfo->ptype), fchdr->oxid,
@@ -1713,7 +1713,7 @@ dissect_fcswils(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void* data)
         /* Opcode is ACC or RJT */
         conversation = find_conversation(pinfo->num, &pinfo->src, &pinfo->dst,
                                          conversation_pt_to_endpoint_type(pinfo->ptype), fchdr->oxid,
-                                         fchdr->rxid, NO_PORT2);
+                                         fchdr->rxid, NO_PORT_B);
         isreq = FC_SWILS_RPLY;
         if (!conversation) {
             if (tree && (opcode == FC_SWILS_SWACC)) {

epan/dissectors/packet-ldss.c
@@ -198,7 +198,7 @@ static void
 prepare_ldss_transfer_conv(ldss_broadcast_t *broadcast)
 {
 	if (!find_conversation(broadcast->num, &broadcast->broadcaster->addr, &broadcast->broadcaster->addr,
-						ENDPOINT_TCP, broadcast->broadcaster->port, broadcast->broadcaster->port, NO_ADDR2|NO_PORT2)) {
+						ENDPOINT_TCP, broadcast->broadcaster->port, broadcast->broadcaster->port, NO_ADDR_B|NO_PORT_B)) {
 		conversation_t *transfer_conv;
 		ldss_transfer_info_t *transfer_info;
 

epan/dissectors/packet-tftp.c
@@ -921,7 +921,7 @@ dissect_tftp(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void* data _U_
     if (!PINFO_FD_VISITED(pinfo)) {
       /* During first pass, look for conversation based upon client port */
       conversation = find_conversation(pinfo->num, &pinfo->src, &pinfo->dst, ENDPOINT_UDP,
-                                       pinfo->destport, 0, NO_PORT2);
+                                       pinfo->destport, 0, NO_PORT_B);
       if (conversation != NULL) {
         /* Set other side of conversation (server port) */
         if (pinfo->destport == conversation_key_port1(conversation->key_ptr))

commit 4606ab6271d0d626c47b2464877a5e79aae53f87

UMTS FP: Fix flag in conversation_new calls
Commit 8020aea27a9673eaae0d3d2333b19767c0d6112c mistakenly changed NO_ADDR_B to NO_PORT2 instead of NO_ADDR2

epan/dissectors/packet-umts_fp.c
@@ -4058,7 +4058,7 @@ set_both_sides_umts_fp_conv_data(packet_info *pinfo, umts_fp_conversation_info_t
         /* Conversation does not exist yet, creating one now. */
         packet_direction_conv = conversation_new(pinfo->num, &pinfo->net_dst, &pinfo->net_src,
             conversation_pt_to_endpoint_type(pinfo->ptype),
-            pinfo->destport, pinfo->srcport, NO_PORT2);
+            pinfo->destport, pinfo->srcport, NO_ADDR2);
     }
     conversation_add_proto_data(packet_direction_conv, proto_fp, umts_fp_conversation_info);
 
@@ -4071,7 +4071,7 @@ set_both_sides_umts_fp_conv_data(packet_info *pinfo, umts_fp_conversation_info_t
         /* Conversation does not exist yet, creating one now. */
         other_direction_conv = conversation_new(pinfo->num, &pinfo->net_src, &pinfo->net_dst,
             conversation_pt_to_endpoint_type(pinfo->ptype),
-            pinfo->srcport, pinfo->destport, NO_PORT2);
+            pinfo->srcport, pinfo->destport, NO_ADDR2);
     }
     conversation_add_proto_data(other_direction_conv, proto_fp, umts_fp_conversation_info);
 